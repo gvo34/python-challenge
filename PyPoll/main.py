f49ee82bf4f412631c225996849e3dc15bc16b90
Guirlyn Olivar <guirlyn@gmail.com>
2018-02-21 13:21:15 -0800

    add multiple input files

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -16,58 +16,83 @@ candidates = []
 candidates_votes = []
 candidates_county = []
 counties = []
+counties_votes = []
 voters = []
 
-voters_file_path = os.path.join('data','election_data_1.csv')
 
-with open(voters_file_path,'r') as election_file:
-  elections = csv.reader(election_file, delimiter=',')
 
-  for vote in elections:
+
+# Add candidate to the list of candidates
+def add_candidate(candidate, candidatelist,votes):
+  candidatelist.append(candidate)
+  cleanlist = list(set(candidatelist))
+  if len(cleanlist) != len(candidatelist): #added a new candidate
+      votes.append(1)
+  return cleanlist
+
+# increase the votecount with matching votelist
+def add_vote(vote, votelist, votecount):
+      try:  #search if the vote is already in the list
+        index = votelist.index(vote)
+      except ValueError:
+        index = -1  
+            # if the vote is in the list, increase the counter
+      if index >= len(votecount) or index == -1:
+        votecount.append(1)
+      else:
+        votecount[index] +=1
+
+
+input_data = ('election_data_1', 'election_data_2')
+
+for election_data in input_data:
+
+  voters_file_path = os.path.join('data',election_data+'.csv')  
+
+  with open(voters_file_path,'r') as election_file:
+    elections = csv.reader(election_file, delimiter=',')
+
+    for vote in elections:
       voter_id = vote[0]
       voters.append(voter_id)
 
       voter_county = vote[1]
-      counties.append(voter_county)
- 
+      counties = add_candidate(voter_county, counties, counties_votes)
+
       voter_candidate = vote[2]
-      try:
-        c_index = candidates.index(voter_candidate)
-      except ValueError:
-        c_index= -1        
+      candidates = add_candidate(voter_candidate, candidates, candidates_votes)
 
-      if c_index != -1:
-         candidates_votes[c_index] += 1
-      else:
-         candidates.append(voter_candidate)
-         candidates_votes.append(1)
-         candidates_county.append(voter_county)
+      add_vote(voter_candidate, candidates, candidates_votes)
+      add_vote(voter_county, counties, counties_votes)
 
-      
+      total = len(voters)
 
-total = len(voters)
 
-percentage = []
-for c in candidates_votes:
+  percentage = []
+  for c in candidates_votes:
     p = float(c)
-    percentage.append((p/total) *100)  
+    percentage.append('{:.0%}'.format((p/total)))  
 
-print("Total candidates tally of :" + str(len(candidates)))
-tally = zip(candidates, percentage, candidates_votes)
-for c in tally:
+  print("----------------- ELECTIONS from "+election_data+" ------------------------")
+  print("Total candidates tally of :" + str(len(candidates)) + " candidates")
+  tally = zip(candidates, percentage, candidates_votes)
+  for c in tally:
     print (c)
 
+  max_votes = max(candidates_votes)
+  m_index = candidates_votes.index(max_votes)
 
+  print(">>>>>>>>>>>>>>>>>>>>>>>>The winner is: " + candidates[m_index])
 
-for county in set(counties):
-     print("County "+county)
-print("total votes "+str(len(voters)))
+  print("Voting partocipation is:")
+  turnout = zip(counties, counties_votes)
+  for t in turnout:
+    print(t)
 
-max_votes = max(percentage)
-m_index = percentage.index(max_votes)
+  max_turnout = max(counties_votes)
+  t_index = counties_votes.index(max_turnout)
 
-print("The winner is " + candidates[m_index])
-
+  print("County with the highest turnout is: "+counties[t_index] + " with " + str(max_turnout)+" votes")
 
 
 
