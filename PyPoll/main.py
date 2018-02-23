62da4b39d4e9b633e4389e2cdb2834d86e859aa0
Guirlyn Olivar <guirlyn@gmail.com>
2018-02-23 12:02:17 -0800

    add tally vote count per county

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -11,63 +11,83 @@
 import os
 import csv
 
-def add_vote(name,votedict):
+def add_vote(name,votedict,vote):
   if name in votedict:
-    votedict[name] +=1
+    votedict[name] +=vote
   else:
-    votedict[name] = 1
+    votedict[name] = vote
 
-candidates = {} # {'candidate': votes}
-counties = {} # {'county': {candidate': votes}}
-total_votes = 0
-totals = {}  # {'county':total-votes}
+
+def county_vote(countyname, candidatename, counties): 
+  if countyname in counties:
+    add_vote(candidatename, counties[countyname],1)
+  else:
+    counties[countyname]={candidatename:1}
+
+def print_votes(total_votes, candidates, counties, report):
+
+  reportfile = os.path.join('output',report+'_report.txt')
+  with open(reportfile,'w',newline='') as reprotf:
+    # Total number of votes
+    print(".................. Elections ..............")
+    print("Total number of votes "+str(total_votes))
+    print("...........................................")
+    #List all candidates
+    for c in candidates:
+      percentage = (candidates[c]/total_votes)*100
+      print(c + " " + str(int(percentage)) +"% ("+ str(candidates[c]) +")")
+      
+    max_key = max(candidates,key=candidates.get)
+    percentage = (candidates[max_key]/total_votes)*100
+    print("...........................................")
+    print("Popular vote winner is "+max_key + " with "+str(int(percentage))+"%")
+      
+    #tally votes
+    tally = {} # {'candidate': all_county_votes}
+    for county in counties:
+      max_candidate = max(counties[county],key=counties[county].get)
+      add_vote(max_candidate, tally, counties[county][max_candidate]) # candidate with max votes for each county
+    winner = max(tally, key=tally.get)
+    print("...........................................")
+    print("WINNER: "+winner)
+    print("...........................................")
+    
+
+def read_elections_report(file_name):
+      # read report from a file                                                                                      
+      reportfile = os.path.join('output',file_name+'_report.txt')
+      with open(reportfile,'r') as reportf:
+            lines = reportf.read()
+            print(lines)
+
+
+def elections(datafiles):
+  for election_file in datafiles:
+    voters_file_path = os.path.join('data',election_file+'.csv')  
+
+    with open(voters_file_path,'r') as election_data:
+      elections = csv.reader(election_data, delimiter=',')
+      next(elections, None) #skip header
+
+      candidates = {} # {'candidate': votes}
+      counties = {}   # {'county': {candidate': votes}}
+      total_votes = 0
+
+      for vote in elections:
+        candidate_name = vote[2]
+        add_vote(candidate_name, candidates,1)
+        total_votes += 1
+        county = vote[1]
+        county_vote(county, candidate_name, counties)
+          
+    print_votes(total_votes, candidates, counties, election_file)  
+    read_elections_report(election_file)
+    
+
+
 
 input_data = ('election_data_1', 'election_data_2')
-
-for election_data in input_data:
-
-  voters_file_path = os.path.join('data',election_data+'.csv')  
-
-  with open(voters_file_path,'r') as election_file:
-    elections = csv.reader(election_file, delimiter=',')
-    next(elections, None) #skip header
-    for vote in elections:
-      candidate_name = vote[2]
-      add_vote(candidate_name, candidates)
-      total_votes += 1
-      county = vote[1]
-
-      if county in counties:
-        add_vote(candidate_name, counties[county])
-        totals[county]+=1
-      else:
-        counties[county]={candidate_name:1}
-        totals[county] = 1
-  
-  # Total number of votes
-  print(".................. Elections ..............")
-  print("Total number of votes "+str(total_votes))
-  print("...........................................")
-  #List all candidates
-  for c in candidates:
-    percentage = (candidates[c]/total_votes)*100
-    print(c + " " + str(int(percentage)) +"% ("+ str(candidates[c]) +")")
-
-  max_key = max(candidates,key=candidates.get)
-  percentage = (candidates[max_key]/total_votes)*100
-  print("...........................................")
-  print("Winner is "+max_key + " with "+str(int(percentage))+"%")
-  print("...........................................")
-
-
-  #tally votes
-#  for county_votes in counties:
-#    max_key = max(counties[county_votes],key=counties[county_votes].get)
-#    percentage = (counties[county_votes][max_key]/totals[county_votes])*100
-#    print("winner in "+county_votes+ " is " + max_key +" with "+str(int(percentage))+"%") 
-
-
-  
+elections(input_data)
 
       
 
